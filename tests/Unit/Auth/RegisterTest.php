0cd9c581fa17599eacd3c542f6061326520be68c
Anmaner <Anmanerr@gmail.com>
2021-01-01 07:12:11 +0200

    Added Auth/Register unit tests

---
+++ tests/Unit/Auth/RegisterTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Unit\Auth;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class RegisterTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function testRegister()
+    {
+        $user = User::register(
+            $name = 'name',
+            $email = 'email',
+            $password = 'password',
+        );
+
+        $this->assertEquals($name, $user->name);
+        $this->assertEquals($email, $user->email);
+        $this->assertNotEquals($password, $user->password);
+
+        $this->assertFalse($user->isVerified());
+
+        $this->assertNotNull($user->verify_token);
+        $this->assertNull($user->email_verified_at);
+    }
+
+    public function testSuccessfulVerify()
+    {
+        $user = User::register(
+            $name = 'name',
+            $email = 'email',
+            $password = 'password',
+        );
+
+        $user->verify($time = Carbon::now());
+
+        $this->assertTrue($user->isVerified());
+        $this->assertNull($user->verify_token);
+        $this->assertEquals($time->toDateTimeString(), $user->email_verified_at);
+    }
+
+    public function testAlreadyVerified()
+    {
+        $user = User::register(
+            $name = 'name',
+            $email = 'email',
+            $password = 'password',
+        );
+
+        $user->verify($time = Carbon::now());
+
+        $this->expectExceptionMessage('User is already verified.');
+
+        $user->verify($time = Carbon::now());
+    }
+}

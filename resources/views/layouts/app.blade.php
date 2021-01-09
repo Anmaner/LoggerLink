5d7f4ef348cb051fdea8ff783a84674d1a255c66
Anmaner <Anmanerr@gmail.com>
2021-01-09 19:31:49 +0200

    CSS Media moved to app.scss and added layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>LoggerLink</title>
+
+    <script src="{{ mix('js/app.js', 'build') }}" defer></script>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="{{ mix('css/app.css', 'build') }}" rel="stylesheet">
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
